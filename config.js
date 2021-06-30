3497e801afb3f6b4b6bca5174fc70e7422c493a9
Hngcanhthucute <86722113+Hngcanhthucute@users.noreply.github.com>
2021-06-30 21:47:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: ' Chú à!',
-    introDesc: `.Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích cháu nè hihi  :> <33',
-    btnNo: 'Không, cháu iu.',
-    question:'Trên thế giới hơn 7 tỉ người mà sao chú lại tò mò vậy kaka:)<3',
-    btnReply: 'Gửi cho Thư cutehihi <3',
-    reply: 'Cháu iu xink dep tuyet vời kh có chỗ che kaka🤣',
-    mess: Chán biết mà =)). Hihi cười đái ',
-    messDesc: 'Tối nay chú lo ngủ sớm đi đó ngày nào cx thấy 12 giờ.',
-    btnAccept: 'Okiiii nèeeee<3',
+    introTitle: 'Chú à!',
+    introDesc: `Đưa tay đay nèo, mãi bên nhau chú nhớ :)).`,
+    btnIntro: 'hihi ok😼',
+    title: 'Mười hai giờ đêm nỗi buồn kia chợt như dài thêm..'
+    desc: 'Chú cũng hay thật, đã khuya rồi mà vẫn chưa ngủ đi? ',
+    btnYes: 'Bây giờ chú ngủ nè',
+    btnNo: 'Không, Cháu iu ',
+    question:'Sao kh ngủ sớm đi, biết cháu lo lâm hong<3',
+    btnReply: 'Gửi cháu cuteohihi',
+    reply: 'Chú hứa là chú sẽ kh ngủ trễ nữa kaka :)',
+    mess: 'kaka hứa thì phải giữ lời thôi chú iu 🤣',
+    messDesc: 'Ngủ sớm đi kh cháu chém =))Kh chịu buộc chịu.',
+    btnAccept: 'Okiiiii lun nhó <3.',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+} 
