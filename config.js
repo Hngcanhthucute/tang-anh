994b0737437b2968e924b5d18f3223ed69991dda
Hngcanhthucute <86722113+Hngcanhthucute@users.noreply.github.com>
2021-06-30 20:38:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: ' Anh à!',
     introDesc: `.Trái đất vốn lạ thường
     Mà sao anh cứ đi nhầm đường
     Lạc vào tim em lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích em đó được chưa :> <33',
-    btnNo: 'Không,e bị đien à😈',
-    question:'Trên thế giới hơn 7 tỉ người mà sao anb lại thích em<3',
-    btnReply: 'Gửi cho tui nè hihi <3',
-    reply: 'Tai vi em xink đẹp tuyet voi',
-    mess: 'Em biết mà 🥰. Cảm ơn nhen 😘😘',
-    messDesc: 'Tối nay ngủ sớm nha anh trai.',
+    btnYes: 'Vẫn cứ là thích em  :> <33',
+    btnNo: 'Không,em à.',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anb lại chọn em<3',
+    btnReply: 'Gửi cho Thư cutehihi <3',
+    reply: 'Tai vi em xink đẹp tuyet voi , ahihi 🤣',
+    mess: 'Em biết mà 🥰. Ngốc ghê á 🤣🤣🤣',
+    messDesc: 'Tối nay mình ngủ sớm nha anh iu .',
     btnAccept: 'Okiiii nè kakaka <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
