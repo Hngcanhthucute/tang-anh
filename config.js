4346e30b71cc680502020d9e3d6cf0f89d9a0025
Hngcanhthucute <86722113+Hngcanhthucute@users.noreply.github.com>
2021-06-30 19:43:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích em <33',
     btnNo: 'Không, em à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
@@ -14,6 +14,6 @@ const CONFIG = {
     reply: 'Chắc do em xink đep tuyet voi <33333333',
     mess: 'Em biết mà 🥰. Ahihihihi đồ ngốc 😘😘',
     messDesc: 'Bữa nào gặp nhau nke :3.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: ' Okii lun haha <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
